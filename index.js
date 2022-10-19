222391744f0637887dc818808df4f05962b4d58c
Varun DeviReddy <varun.d.reddy@gmail.com>
2022-10-19 13:03:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const config = require('config')
 
 app.get('/', (req, res)=>{
-    res.send('CI/CD App, Yay, Works well !')
+    res.send('CI/CD App, Yay Varun, Works well !')
 })
 
 app.get('/status', (req, res)=>{
